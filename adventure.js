168c698ca5b84b07f07c81da928acfeb58cec264
zpilia <61697702+zpilia@users.noreply.github.com>
2022-06-30 19:13:17 +0200

    modif adventure

---
+++ adventure.js
@@ -0,0 +1,9 @@
+//fonction pour obtenir un nbr aléatoire et faire deviner à 'lutilisateur
+//si c'est faux, on recommence le jeu
+
+function devine() {
+    let aleaNombre = Math.random();
+    console.log(aleaNombre);
+}
+
+devine();
